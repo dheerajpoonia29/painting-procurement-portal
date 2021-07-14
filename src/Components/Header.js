47abbb1d9882447596758cb929eadcda1a47379c
Rishi Dutt Shukla <51123074+rduttshukla@users.noreply.github.com>
2021-07-14 16:56:10 +0530

    Started integration of Helio template

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "../css/header.css";
+import "../css/helio.css";
 
 class Header extends Component {
   constructor(props) {
@@ -7,15 +7,15 @@ class Header extends Component {
   }
   render() {
     return (
-      <div className="headerContainer">
+      <header id="header">
         <div style={{ float: "left" }}>
-          <h1 className="logoTitle">Painting Procurement</h1>
+          <h1 className="logo" style={{color: "white", fontSize: '22px', marginTop:'7px', marginLeft: '10px'}}>Painting Procurement</h1>
         </div>
         <div style={{ float: "right" }}>
-          <h1 className="accInfo">{this.props.account} | <span style={{
+          <h1 style={{color: "white", fontSize: '22px', marginTop:'7px', marginRight: '10px'}}>{this.props.account} | <span style={{
     border: "2px dashed #f69c55"}}>{this.props.balance} Eth</span> </h1>
         </div>
-      </div>
+      </header>
     );
     }
 }
